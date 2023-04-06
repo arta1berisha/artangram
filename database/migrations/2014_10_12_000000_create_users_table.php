969a67eed34e6d5d8c22b39dd57ab76274b8ab34
arta1berisha <ab52558@ubt-uni.net>
2023-04-06 17:12:29 +0200

    fixed bug on the 'is_private'

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('is_private')->byDefault('false');
+            $table->boolean('is_private')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
