d654880bed5f7a96ff72a646241e59a9b6cb0052
arta1berisha <ab52558@ubt-uni.net>
2023-04-06 17:06:40 +0200

    Seeders and Factories added for User and Post

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            // 'slug' => fake()->slug(),
+            // 'title' => fake()->title(),
+            // 'post_image' => fake()->image(),
+            // 'description' => fake()->paragraph(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'test',
+            'username' => 'test',
+            'email' => 'test@gmail.com',
+            'password' => '12345678',
+            'is_private' => '1'
+        ]);
     }
+    //     \App\Models\Post::factory(10)->create();
+
+    //     \App\Models\Post::factory()->create([
+    //         'slug' => 'test-test',
+    //         'post_img' => 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FLaravel&psig=AOvVaw1eD5E_fMz5QNSBEsY5l5lw&ust=1680858919579000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCMCV4Mj1lP4CFQAAAAAdAAAAABAD',
+    //         'description' => 'lorem ipsum',
+    //     ]);
+    // }
 }
