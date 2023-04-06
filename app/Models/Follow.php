6ce7fb6300ad6916ad4c30bccd1b410c455aebe4
arta1berisha <ab52558@ubt-uni.net>
2023-04-06 17:08:32 +0200

    Relationships of Follow added

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\FollowingStatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,48 +10,20 @@ class Follow extends Model
 {
     use HasFactory;
 
-    protected function followersOfThisUser()
-    {
-        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'following_id')
-        ->withPivot('status')
-        ->wherePivot('status', 'confirmed');
-    }
-
-    protected function thisUserFollowerOf()
-    {
-        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'following_id')
-        ->withPivot('status')
-        ->wherePivot('status', 'confirmed');
-    }
-
-    public function getFollowersAttributes()
-    {
-        if ( ! array_key_exists('followers', $this->relations)) $this->loadFollowers();
-        return $this->getRelation('followers');
-    }
-
-    protected function loadFollowers()
-    {
-        if ( ! array_key_exists('followers', $this->relations))
-        {
-            $followers = $this->mergeFollowers();
-            $this->setRelation('followers', $followers);
-        } else {
-            return response()->json(['message' => 'You havent any followers']);
-        }
-    }
-
-    protected function mergeFollowers()
-    {
-        if($temp = $this->followersOfThisUser)
-        return $temp->merge($this->thisUserFollowerOf);
-        else
-        return $this->thisUserFriendOf();
-    }
-
+    protected $fillable = [
+    ];
     public function followers()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'following_id')
+            ->withTimestamps()
+            ->withPivot('status');
+    }
+
+    public function following()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'following_id', 'follower_id')
+            ->withTimestamps()
+            ->withPivot('status');
     }
 
 }
