4c6d2a8d64ecb4844de459add288846bf789e4c3
arta1berisha <ab52558@ubt-uni.net>
2023-04-06 17:13:16 +0200

    Added an Enum for 'status' in users table

---
+++ database/migrations/2023_04_03_133305_add_status_to_followers_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Enums\FollowingStatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('followers', function (Blueprint $table) {
+            $table->ENUM('status', ['Accepted', 'Pending', 'Rejected'])->after('following_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('followers', function (Blueprint $table) {
+            //
+        });
+    }
+};
