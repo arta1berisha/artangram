2cd9315081d8472a8fbf7179442e59facc1b8b50
arta1berisha <ab52558@ubt-uni.net>
2023-04-14 13:31:25 +0200

    Post Policy made for the methods

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -4,59 +4,45 @@ namespace App\Policies;
 
 use App\Models\Post;
 use App\Models\User;
-use App\Traits\ApiResponseTrait;
 use Illuminate\Auth\Access\Response;
 
 class PostPolicy
 {
-    use ApiResponseTrait;
-
     public function before(User $user, string $ability): bool|null
     {
-        if ($user->isAdministrator()) {
+        if ($user->isAdmin()) {
             return true;
         }
 
         return null;
     }
+
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $follower): bool
+    public function viewAny(User $user): bool
     {
-        /** @var User */
-        $user = auth()->user();
-
-         $canViewPost = $user
-            ->followers()
-            ->where('status', 'Accepted')
-            ->where('follower_id', $follower->id);
-
-            return $canViewPost;
+        return true;
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $follower): bool
+    public function view(User $user, Post $post): bool
     {
-           /** @var User */
-           $user = auth()->user();
-        
-           $canViewPost = $user
-              ->followers()
-              ->where('status', 'Accepted')
-              ->where('follower_id', $follower->id);
-  
-              return $canViewPost;
+        return $user
+            ->following()
+            ->where('status', 'Accepted')
+            ->where('following_id', $post->user_id)
+            ->exists();
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user, Post $post): bool
+    public function create(User $user): bool
     {
-        return $user->id === $post->user_id;
+        return true;
     }
 
     /**
@@ -72,22 +58,23 @@ class PostPolicy
      */
     public function delete(User $user, Post $post): bool
     {
+
         return $user->id === $post->user_id;
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    // public function restore(User $user, Post $post): bool
-    // {
-    //     //
-    // }
+    public function restore(User $user, Post $post): bool
+    {
+        return true;
+    }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    // public function forceDelete(User $user, Post $post): bool
-    // {
-    //     //
-    // }
+    public function forceDelete(User $user, Post $post): bool
+    {
+        return true;
+    }
 }
